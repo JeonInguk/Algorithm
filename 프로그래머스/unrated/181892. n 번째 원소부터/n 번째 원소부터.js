a5eb7a9016eaf164947ff3e23996c9671da2e2a3
JeonInguk <117060240+JeonInguk@users.noreply.github.com>
2023-08-05 23:57:42 +0900

    [unrated] Title: n 번째 원소부터, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181892. n 번째 원소부터/n 번째 원소부터.js
@@ -0,0 +1,9 @@
+function solution(num_list, n) {
+    const answer = [];
+    for (let i = 0; i < num_list.length; i++) {
+        if (i >= n - 1) {
+            answer.push(num_list[i]);
+        }
+    }
+    return answer;
+}
